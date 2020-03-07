a287f9f3390e23dd17f6139b7613050819ddfef7
HasanMammadov <hasanmamedoff@gmail.com>
2020-03-07 16:44:22 -0500

    day 5. nested Frames

---
+++ src/test/java/day5/TestForIFrame.java
@@ -0,0 +1,42 @@
+package day5;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestForIFrame {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/iframe");
+        BrowserUtils.wait(4);
+
+        //before looking for that element, we need to jump to that frame
+        //you can specify: name, id, index or webelement of the frame
+        //it's like we are jumping to another layer
+        driver.switchTo().frame("mce_0_ifr");// 1st step. go to frame.
+
+        //now, this content will be visible
+        WebElement textInput = driver.findElement(By.id("tinymce"));// second. find element
+
+        System.out.println(textInput.getText());
+
+        BrowserUtils.wait(4);
+
+        textInput.clear();//to delete text
+        textInput.sendKeys("Hello, World!");// manipulating frame content
+
+        BrowserUtils.wait(4);
+
+        //exit from the frame
+        driver.switchTo().defaultContent();
+
+        WebElement heading = driver.findElement(By.tagName("h3"));
+        System.out.println(heading.getText());
+
+        driver.quit();
+    }
+}
