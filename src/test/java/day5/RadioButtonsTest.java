de6554164010591362570c5fc22905dff99ce79e
HasanMammadov <hasanmamedoff@gmail.com>
2020-03-07 12:43:07 -0500

    day 5

--- src/test/java/day5/RadioButtonsTest.java
+++ src/test/java/day5/RadioButtonsTest.java
@@ -1,28 +1,29 @@
 package day5;
-
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 public class RadioButtonsTest {
     public static void main(String[] args) {
+//        if DriverFactory doesn't work, use this:
+//        WebDriverManager.chromedriver().version("79").setup();
+//        WebDriver driver = new ChromeDriver();
         WebDriver driver = DriverFactory.createDriver("chrome");
-
         driver.get("http://practice.cybertekschool.com/radio_buttons");
-        driver.manage().window().maximize();
-
-        BrowserUtils.wait(2000);
-
-        WebElement blackElement = driver.findElement(By.id("black"));
-
-
-
-
-
-
+        BrowserUtils.wait(2);
+        //<input type="radio" id="black" name="color">
+        WebElement blackButton = driver.findElement(By.id("black"));
+        blackButton.click();
+        //how do we verify that button clicked
+        //returns true, if button clicked
+        if(blackButton.isSelected()){
+            System.out.println("TEST PASSED!");
+        }else {
+            System.out.println("TEST FAILED");
+        }
         driver.quit();
-
-          }
+    }
 }
