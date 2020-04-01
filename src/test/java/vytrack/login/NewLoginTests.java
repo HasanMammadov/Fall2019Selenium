6982651678207d2f8e09fc851d35d7b9b0ab4576
HasanMammadov <hasanmamedoff@gmail.com>
2020-03-31 21:59:39 -0400

    Browser utilts,Screen Shot

--- src/test/java/vytrack/login/NewLoginTests.java
+++ src/test/java/vytrack/login/NewLoginTests.java
@@ -11,19 +11,28 @@ public class NewLoginTests extends AbstractTestBase {
     @Test
     public void verifyPageTitle(){
 
+
+        test = report.createTest("Verify page title");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
 
-        Assert.assertEquals(Driver.getDriver().getTitle(),"Dash");
+        test.info("Login as a store manager");
+
+        Assert.assertEquals(Driver.getDriver().getTitle(),"Dashboard");
+        test.pass("Page title Dashboard was verified");
     }
 
 
     @Test
     public void verifyWarningMessage(){
+        test = report.createTest("Verify page title");
+
         LoginPage loginPage = new LoginPage();
         loginPage.login();
         Assert.assertEquals(loginPage.getWarningMessageText(),"Invalid user name or password.");
         BrowserUtils.getScreenshot("warning_message");
+        test.pass("Warning message is displayed");
+
 
     }
 }
